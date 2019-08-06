d8070e33f5f80bbf0fc080a341c121817b399000
Frankzhac <Frank.hemle@gmail.com>
2019-08-05 23:13:04 -0400

    landing page header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content} from "react-mdl";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div style={{height: '300px', position: 'relative'}}>
+        <Layout fixedHeader>
+            <Header title={<span><span style={{ color: '#ddd' }}></span><strong>My Portfolio</strong></span>}>
+                <Navigation>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                </Navigation>
+            </Header>
+            <Drawer title="Title">
+                <Navigation>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                </Navigation>
+            </Drawer>
+            <Content />
+        </Layout>
     </div>
   );
 }
