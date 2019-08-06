8b7e10e8848653f229c10ba74a3af12a5adb9526
Frankzhac <Frank.hemle@gmail.com>
2019-08-05 23:29:40 -0400

    components set up

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content} from "react-mdl";
 
-function App() {
-  return (
-     <div style={{height: '300px', position: 'relative'}}>
-        <Layout fixedHeader>
-            <Header title={<span><span style={{ color: '#ddd' }}></span><strong>My Portfolio</strong></span>}>
-                <Navigation>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                </Navigation>
-            </Header>
-            <Drawer title="Title">
-                <Navigation>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                </Navigation>
-            </Drawer>
-            <Content />
-        </Layout>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div style={{height: '300px', position: 'relative'}}>
+          <Layout fixedHeader>
+              <Header title={<span><span style={{ color: '#ddd' }}></span><strong>My Portfolio</strong></span>}>
+                  <Navigation>
+                      <a href="/">Resume</a>
+                      <a href="/">Link</a>
+                      <a href="/">Link</a>
+                      <a href="/">Link</a>
+                  </Navigation>
+              </Header>
+              <Drawer title="Title">
+                  <Navigation>
+                      <a href="/">Link</a>
+                      <a href="/">Link</a>
+                      <a href="/">Link</a>
+                      <a href="/">Link</a>
+                  </Navigation>
+              </Drawer>
+              <Content />
+          </Layout>
+      </div>
+    );
+  } 
 }
 
 export default App;
